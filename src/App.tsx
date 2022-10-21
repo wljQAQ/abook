c383d47bf9c9384b8033c4378ee0f7e8d3827c5b
wulongjiang <562445380@qq.com>
2022-10-21 14:28:37 +0800

    feat: 继承react路由

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@
  * @Author: wlj
  * @Date: 2022-10-20 14:43:15
  * @LastEditors: wlj
- * @LastEditTime: 2022-10-20 17:19:50
+ * @LastEditTime: 2022-10-21 14:16:53
  * @Description:
  */
-import './App.css';
+import { memo, Suspense } from 'react';
+import { useRoutes } from 'react-router-dom';
+import routes from '@/router/index';
 
-function App() {
-  const a = 1;
-  console.log(a);
-  return <div className="App">1122211</div>;
-}
+const App = memo(() => {
+  return (
+    <div className="app">
+      <header>header</header>
+      <Suspense fallback="loading">{useRoutes(routes)}</Suspense>
+      <footer>footer</footer>
+    </div>
+  );
+});
+
+App.displayName = 'App';
 
 export default App;
---
+++ src/views/Home.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: wlj
+ * @Date: 2022-10-21 09:46:53
+ * @LastEditors: wlj
+ * @LastEditTime: 2022-10-21 14:26:49
+ * @Description: 主页面
+ */
+
+import { memo } from 'react';
+
+const Home = memo(() => {
+  return <div>Home</div>;
+});
+
+Home.displayName = 'Home';
+
+export default Home;
