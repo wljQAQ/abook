c928f904512be315a291f5cc08cc955f7efb85e2
wulongjiang <562445380@qq.com>
2022-10-21 16:04:49 +0800

    feat: 集成Immer

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,14 +2,44 @@
  * @Author: wlj
  * @Date: 2022-10-21 09:46:53
  * @LastEditors: wlj
- * @LastEditTime: 2022-10-21 14:26:49
+ * @LastEditTime: 2022-10-21 16:02:51
  * @Description: 主页面
  */
 
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import { useImmer } from 'use-immer';
 
 const Home = memo(() => {
-  return <div>Home</div>;
+  const [test, setTest] = useState({
+    a: '123',
+    b: {
+      c: '456'
+    }
+  });
+
+  const [test2, setTest2] = useImmer({
+    a: '123',
+    b: {
+      c: '456'
+    }
+  });
+  return (
+    <>
+      <div>Home</div>
+      <div>{JSON.stringify(test)}</div>
+      <div>{JSON.stringify(test2)}</div>
+      <button onClick={() => setTest({ ...test, a: '456' })}>click</button>
+      <button
+        onClick={() =>
+          setTest2(draft => {
+            draft.a = '456';
+          })
+        }
+      >
+        Immerclick
+      </button>
+    </>
+  );
 });
 
 Home.displayName = 'Home';
