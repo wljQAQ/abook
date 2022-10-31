e72be37d1682041e37077366ae45fc053357da1e
wulongjiang <562445380@qq.com>
2022-10-31 15:51:05 +0800

    feat: 集成tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@
  * @Author: wlj
  * @Date: 2022-10-20 14:43:15
  * @LastEditors: wlj
- * @LastEditTime: 2022-10-28 10:46:50
+ * @LastEditTime: 2022-10-31 14:30:39
  * @Description:
  */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteEslint from 'vite-plugin-eslint';
-import { getThemeVariables } from 'antd/dist/theme';
+import vitePluginImp from 'vite-plugin-imp';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
@@ -17,6 +17,15 @@ export default defineConfig({
     react(),
     viteEslint({
       failOnError: false
+    }),
+    vitePluginImp({
+      optimize: true,
+      libList: [
+        {
+          libName: 'antd',
+          style: name => `antd/es/${name}/style`
+        }
+      ]
     })
   ],
   resolve: {
@@ -29,10 +38,9 @@ export default defineConfig({
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
-        // modifyVars: getThemeVariables({
-        //   // '@primary-color': 'red' // 全局主色
-        //   dark: true // 开启暗黑模式
-        // })
+        modifyVars: {
+          hack: `true; @import "./src/assets/style/theme.less";`
+        }
       }
     }
   }
