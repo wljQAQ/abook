49d0243515e2284add8bd0a0698b639068e70e2b
wulongjiang <562445380@qq.com>
2022-11-13 17:43:10 +0800

    feat: 新增创建知识库时的背景图

---
+++ src/views/BookSpace/children/Detail/index.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: wulongjiang
+ * @Date: 2022-11-13 16:53:21
+ * @LastEditors: wulongjiang
+ * @LastEditTime: 2022-11-13 16:54:10
+ * @Description:
+ * @FilePath: \blog\src\views\BookSpace\children\Detail\index.tsx
+ */
+import React, { memo } from 'react';
+
+const BookDetail = memo(() => {
+  return <div>BookDetail</div>;
+});
+
+BookDetail.displayName = 'BookDetail';
+
+export default BookDetail;
--- src/views/BookSpace/index.tsx
+++ src/views/BookSpace/index.tsx
@@ -2,16 +2,41 @@
  * @Author: wulongjiang
  * @Date: 2022-11-04 20:42:04
  * @LastEditors: wulongjiang
- * @LastEditTime: 2022-11-12 12:13:12
+ * @LastEditTime: 2022-11-13 17:40:34
  * @Description:新建一个知识库页面
  * @FilePath: \blog\src\views\BookSpace\index.tsx
  */
 import { memo } from 'react';
+import BookDetail from './children/Detail';
+// const bgMap = import.meta.glob('../../assets/img/background/*.jpg');
+// let bgArr = [];
+
+// for (const key in bgMap) {
+//   if (Object.prototype.hasOwnProperty.call(bgMap, key)) {
+//     bgArr.push(bgMap[key]);
+//   }
+// }
+
+// console.log(bgArr);
 
 const BookSpace = memo(() => {
+  // const bgImgIndex = Math.ceil(Math.random() * 7);
+  console
+    .log
+    // bgArr[bgImgIndex]().then(res => {
+    //   console.log(res);
+    // })
+    // bgArr[bgImgIndex]().then(res => {
+    //   console.log(res);
+    // }),
+    // bgArr
+    ();
   return (
-    <div className="flex w-full h-full ">
-      <div className="h-full"> 11</div>
+    <div className="flex w-full h-full">
+      {/* <img src={lazy(() => bgArr[bgImgIndex])} alt="" /> */}
+      <div className="h-full">
+        <BookDetail></BookDetail>
+      </div>
     </div>
   );
 });
