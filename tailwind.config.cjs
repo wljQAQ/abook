97823ebce2611d07dfc5539f8b3efb5b633cb134
wulongjiang <562445380@qq.com>
2022-11-20 20:23:52 +0800

    fix: 解决tailwind 与 antd 样式冲突

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /*
  * @Author: wlj
  * @Date: 2022-10-31 15:37:23
- * @LastEditors: wlj
- * @LastEditTime: 2022-11-15 08:16:35
+ * @LastEditors: wulongjiang
+ * @LastEditTime: 2022-11-20 20:22:34
  * @Description:
  */
 /** @type {import('tailwindcss').Config} */
@@ -15,5 +15,8 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [],
+  corePlugins: {
+    preflight: false // <== disable this!
+  }
 };
