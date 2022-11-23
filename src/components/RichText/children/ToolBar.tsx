d76273f248323264f5b02d712c6dbd45fc5f0aed
wulongjiang <562445380@qq.com>
2022-11-23 17:32:53 +0800

    feat: 迷茫

--- src/components/RichText/children/ToolBar.tsx
+++ src/components/RichText/children/ToolBar.tsx
@@ -2,7 +2,7 @@
  * @Author: wlj
  * @Date: 2022-11-17 16:42:44
  * @LastEditors: wlj
- * @LastEditTime: 2022-11-22 17:36:56
+ * @LastEditTime: 2022-11-23 17:22:05
  * @Description:
  */
 import { memo, RefObject, useEffect } from 'react';
@@ -26,22 +26,29 @@ interface ToolBarProps {
 
 const headerMeau: MenuProps['items'] = [
   {
-    key: '',
-    label: <p>正文</p>
-  },
-  {
     key: '1',
     label: <h1>标题1</h1>
   },
   {
     key: '2',
-    label: <h1>标题2</h1>
+    label: <h2>标题2</h2>
   },
   {
     key: '3',
-    label: <h1>标题3</h1>
+    label: <h3>标题3</h3>
+  },
+  {
+    key: '',
+    label: <p>正文</p>
   }
 ];
+
+// let Inline = Quill.import('blots/inline');
+// class BoldBlot extends Inline {}
+// BoldBlot.blotName = 'bold';
+// BoldBlot.tagName = 'strong';
+
+// Quill.register(BoldBlot);
 
 let quill: InstanceType<typeof Quill>; //quill实例对象
 
@@ -62,6 +69,10 @@ const ToolBar = memo(({ quillRef }: Properties) => {
     console.log(quill);
     quill.format('header', value.key);
   };
+
+  const test = function () {
+    quill.format('bold', true);
+  };
   useEffect(() => {
     if (quillRef?.current && typeof quillRef?.current?.getEditor === 'function') {
       quill = quillRef?.current?.getEditor();
@@ -70,27 +81,40 @@ const ToolBar = memo(({ quillRef }: Properties) => {
   return (
     <div id="toolbar" style={{ display: 'flex', alignItems: 'center' }}>
       {/* 设置标题字体 */}
-      <Dropdown
+      {/* <Dropdown
         menu={{ items: headerMeau, onClick: dropSelect }}
         placement="bottomLeft"
         trigger={['click']}
       >
-        <Button className=" !w-14 hover:!bg-gray-100 !text-black" value={toolBar.header.value}>
+        <Button
+          className=" !w-14 shadow-none hover:!bg-gray-100 !text-black"
+          value={toolBar.header.value}
+        >
           <span>{toolBar.header.label}</span>
           <CaretDownOutlined className="text-gray-500 text-xs"></CaretDownOutlined>
         </Button>
-      </Dropdown>
+      </Dropdown> */}
+      {/* <select className="ql-header" defaultValue={''}>
+        <option value="1" />
+        <option value="2" />
+        <option value="3" />
+        <option value="4" />
+        <option value="5" />
+        <option value="" />
+      </select> */}
       {/* 加粗 */}
-      <ToolBarBtn className="ql-bold" tip="加粗"></ToolBarBtn>
-      <Button className="ql-italic"></Button>
-      <select className="ql-color" defaultValue={''}>
+      {/* <ToolBarBtn className="ql-bold" tip="加粗"></ToolBarBtn> */}
+      <Button className="ql-italic" onClick={test}>
+        B
+      </Button>
+      {/* <select className="ql-color" defaultValue={''}>
         <option value="red"></option>
         <option value="green"></option>
         <option value="blue"></option>
         <option value="orange"></option>
         <option value="violet"></option>
         <option value="#d0d1d2"></option>
-      </select>
+      </select> */}
     </div>
   );
 });
--- src/components/RichText/index.tsx
+++ src/components/RichText/index.tsx
@@ -2,13 +2,14 @@
  * @Author: wulongjiang
  * @Date: 2022-10-30 12:47:25
  * @LastEditors: wlj
- * @LastEditTime: 2022-11-22 17:42:23
+ * @LastEditTime: 2022-11-23 17:13:58
  * @Description: 富文本编辑器
  * @FilePath: \blog\src\components\RichText\index.tsx
  */
 import ReactQuill from 'react-quill';
 import { ReactQuillProps } from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+// import 'react-quill/dist/quill.snow.css';
+import './index.less';
 import { memo, useRef, useState } from 'react';
 import ToolBar from '@/components/RichText/children/ToolBar';
 const modules = {
@@ -41,20 +42,36 @@ const RichText = memo(() => {
   const handleQuillChange: ReactQuillProps['onChange'] = function (value, delta, source, editor) {
     console.log(value, delta, source, editor, 'editor change');
   };
+  const handleQuillFocus: ReactQuillProps['onFocus'] = function (selection, source, editor) {
+    console.log(selection, source, editor, 'editor focus');
+  };
+  const handleQuillSelection: ReactQuillProps['onChangeSelection'] = function (
+    selection,
+    source,
+    editor
+  ) {
+    console.log(selection, source, editor, 'editor onChangeSelection');
+  };
 
   return (
     <>
-      <ToolBar quillRef={quillRef}></ToolBar>
-      <ReactQuill
-        ref={quillRef}
-        className="h-full"
-        theme="snow"
-        placeholder="111111"
-        value={value}
-        modules={modules}
-        formats={formats}
-        onChange={handleQuillChange}
-      ></ReactQuill>
+      <div className="flex flex-col h-full">
+        <ToolBar quillRef={quillRef}></ToolBar>
+        <ReactQuill
+          ref={quillRef}
+          className="flex-1"
+          placeholder="test"
+          value={value}
+          theme={''}
+          // modules={modules}
+          // formats={formats}
+          onChange={handleQuillChange}
+          onFocus={handleQuillFocus}
+          onChangeSelection={handleQuillSelection}
+        >
+          <div className="my-editing-area relative h-full w-full border border-soild"></div>
+        </ReactQuill>
+      </div>
     </>
   );
 });
