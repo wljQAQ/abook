4948a055dd80367b7797c1580c9c8c992788a7fe
wulongjiang <562445380@qq.com>
2023-04-20 16:18:20 +0800

    feat: 类型更新

--- src/http/api/home/type.d.ts
+++ src/http/api/home/type.d.ts
@@ -1,19 +1,18 @@
 /*
  * @Author: wlj
  * @Date: 2023-04-08 17:17:59
- * @LastEditors: wulongjiang
- * @LastEditTime: 2023-04-08 18:09:56
+ * @LastEditors: wlj
+ * @LastEditTime: 2023-04-20 16:17:13
  * @Description:
  */
-export type GetBooksRes =
-  | [
-      {
-        id: number;
-        title: string;
-        userId: number;
-        description: string;
-        createdAt: string;
-        updatedAt: string;
-      },
-    ]
-  | [];
+
+interface Book {
+  id: number;
+  title: string;
+  userId: number;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type GetBooksRes = Book[] | [];
